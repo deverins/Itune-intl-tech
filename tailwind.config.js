c1ab42662941e4bd6e7ecaf3088ebfecdf5d1f3c
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-07-06 23:38:42 +0100

    Added a font to use for the project.Which is

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,29 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Montserrat: ['Montserrat', 'sans-serif'],
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    function({ addUtilities }) {
+      const newUtilities = {
+        '.montserrat-regular': {
+          fontFamily: 'Montserrat, sans-serif',
+          fontOpticalSizing: 'auto',
+          fontWeight: '400',
+          fontStyle: 'normal',
+        },
+        '.montserrat-bold': {
+          fontFamily: 'Montserrat, sans-serif',
+          fontOpticalSizing: 'auto',
+          fontWeight: '700',
+          fontStyle: 'normal',
+        },
+      }
+      addUtilities(newUtilities, ['responsive', 'hover'])
+    }
+  ],
 }
