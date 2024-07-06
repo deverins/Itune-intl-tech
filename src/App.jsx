5c4c3b99fbb57d98fa5a21a8f2e3b8830088ee76
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-07-06 23:04:26 +0100

    First commit after initialization.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div className=' h-full bg-black text-cursor text-white '>
+          goods
+      </div>
+    </>
+  )
+}
+
+export default App
