3dadd5142fd3939a0982114f5d8dea2596bf8d14
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-07-13 14:30:48 +0100

    Implement fetchProducts utility function with Axios for fetching product data

--- src/utils/fetchProducts .js
+++ src/utils/fetchProducts .js
@@ -2,12 +2,21 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 import { API_URL } from '../Components/constants/Api';
 
-export const fetchProducts = async (setProducts, setLoading) => {
-  const URL = `${API_URL}`;
+export const fetchProducts = async (setProducts, setLoading, currentPage = 1) => {
+  setLoading(true);
   try {
-    const { data } = await axios.get(`/api/${URL}`);
-    console.log('data response', data);
-    setProducts(data.items);
+    const response = await axios.get(API_URL, {
+      params: {
+        organization_id: import.meta.env.VITE_ORGANIZATION_ID,
+        reverse_sort: false,
+        page: currentPage,
+        size: 10,
+        Appid: import.meta.env.VITE_APP_ID,
+        Apikey: import.meta.env.VITE_API_KEY,
+      },
+    });
+    const data = response.data.items;
+    setProducts(data);
   } catch (error) {
     console.error('Error fetching products:', error);
     toast.error('Failed to fetch products.');
